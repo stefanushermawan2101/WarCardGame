b57ea7f5869f9dd984b7a02b95c9746e04d40d92
Stefanus Hermawan Sebastian <ssebastian22@bsd.idserve.net>
2022-03-25 21:21:49 +0700

    Initial Commit

---
+++ WarCardGame/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WarCardGame
+//
+//  Created by Stefanus Hermawan Sebastian on 25/03/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
